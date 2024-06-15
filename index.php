e4f3e7094ddc12268cec10e52c8210dc5bd6abeb
nkgprojects <nkgsocialsite@gmail.com>
2024-06-15 09:34:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resume</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body style="width:90%;">
+    <div class="resume">
+        <div class="header">
+            <h1 style="color:#0083b4;">Neeraj Kumar Gupta</h1>
+            <p>Senior Software Engineer</p>
+            <p>Email: nkg8352@gmail.com | Phone: +91 92345 93122 | https://tinyurl.com/neerajkumarguptacv </p>
+        </div>
+        <div class="section">
+            <h2>Overview</h2>
+            <div class="skills">
+               <p>6.5 years of experience in Software Development. Effective and proactive team member with ability to handle project
+from requirement gathering, planning and project execution. Appreciated for proactive approach towards problem 
+solvingand completing project within strict time and other constraints.
+</p>
+            </div>
+        </div>
+        <div class="section">
+            <h2>Skills</h2>
+            <div class="skills">
+                <ul>
+                    <li>ASP.Net</li>
+                    <li>.Net Core</li>
+                    <li>ASP.Net MVC</li>
+                    <li>Web API</li>
+                    <li>Entity Framewrok</li>
+                    <li>SQL Server</li>
+                    <li>Azure Devops</li>
+                    <li>Azure Storage</li>
+                    <li>Azure Functions</li>
+                    <li>Azure Key-Vault</li>
+                    <li>Version Control (Git)</li>
+                    <li>Docker</li>
+                </ul>
+            </div>
+        </div>
+         <div class="section">
+            <h2>Career Contour</h2>
+            <div class="education">
+                <div class="degree">
+                    <p>IBM     Pune,    Apr 2017 - June 2022</p>
+                    <p>TCS     Kolkata  Jun 2022 - Oct 2023</p>
+                    <p>Zensar  Pune     Nov 2023 - Mar 2024</p>
+                </div>
+            </div>
+        </div>
+        <div class="section">
+    <h2>Experience</h2>
+    <div class="experience">
+        <div class="job">
+            <h3>Zensar [Nov 2023 - Mar 2024]</h3>
+            <h4>Senior Software Developer | Assurant [USA]</h4>
+            <ul>
+    <li>Leading L2 support for Assurant project, ensuring smooth operation and timely resolution of issues in ASP.NET and .Net Core applications deployed on Azure.</li></br>
+    <li>Enhancing existing applications to meet evolving business requirements, implementing new features, and optimizing performance for enhanced user experience.</li></br>
+    <li>Managing daily tickets, prioritizing tasks, and coordinating with team members to ensure timely resolution and effective communication with stakeholders.</li></br>
+    <li>Providing technical guidance and mentorship to junior team members, fostering a collaborative and growth-oriented work environment.</li></br>
+    <li>Collaborating with cross-functional teams including development, testing, and product management to deliver high-quality solutions aligned with project objectives.</li>
+</ul>
+
+
+            <h4>Roles And Responsibilities</h4>
+            <ul>
+                <li>Addressing daily tickets, delivering enhancements, and managing a small team.</li>
+            </ul>
+        </div>
+
+        <div class="job">
+            <h3>TCS [Jun 2022 - Oct 2023]</h3>
+            <h4>IT Analyst | American Electric Power [USA]</h4>
+            <ul>
+    <li>Providing L2 support for American Electric Power, ensuring uninterrupted operation and swift resolution of technical issues in Microsoft technology-based applications.</li></br>
+    <li>Collaborating with stakeholders to understand business requirements and implement enhancements to optimize application functionality and performance.</li></br>
+    <li>Managing daily tickets, prioritizing tasks based on severity and impact, and communicating effectively with end-users to provide timely resolutions and updates.</li></br>
+    <li>Conducting root cause analysis for recurring issues, implementing preventive measures, and documenting solutions for future reference.</li></br>
+    <li>Participating in regular meetings and knowledge-sharing sessions to stay updated on project developments and contribute to continuous improvement initiatives.</li>
+</ul>
+
+            <h4>Roles And Responsibilities</h4>
+            <ul>
+                <li>Addressing daily tickets and delivering enhancements.</li>
+            </ul>
+        </div>
+
+        <div class="job">
+            <h3>IBM [Apr 2017 - Jun 2022]</h3>
+            <h4>Application Developer | Honda [Europe]</h4>
+            <ul>
+    <li>Developed and implemented the Parts Sales reporting system, providing stakeholders with comprehensive insights into sales performance and inventory management.</li></br>
+    <li>Led the requirements gathering process, collaborating with stakeholders to understand business needs and translate them into technical specifications.</li></br>
+    <li>Designed and optimized the database structure to ensure efficient data storage, retrieval, and reporting capabilities, enhancing system performance and scalability.</li></br>
+    <li>Coordinated with cross-functional teams including developers, testers, and business analysts to ensure seamless integration and alignment with project objectives.</li></br>
+    <li>Oversaw the maintenance of enhancements, conducting regular updates, bug fixes, and performance optimizations to ensure the system's continued functionality and relevance.</li>
+</ul>
+
+            <h4>Roles And Responsibilities</h4>
+            <ul>
+                <li>Ensuring successful integration and functionality of enhancements.</li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+         <div class="section">
+            <h2>Certifications</h2>
+            <div class="skills">
+               
+                <P>AZ  900 [Azure Fundamentals ]</P>
+                <P>AZ 204 [Developing Solutions for Azure]</P>
+              
+            </div>
+        </div>
+        <div class="section">
+            <h2>Education</h2>
+            <div class="education">
+                <div class="degree">
+                    <h3>Bachelor of Science in Computer Science</h3>
+                    <p>Gandhi Engineering College [BPUT]</p>
+                    <p>Graduated: May 2016</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
